5ee5f1d0770d0f3490ec1c5a21c4cb6280e1b1a4
Yan Ren <ryan1219@live.com>
2016-09-16 19:28:02 -0400

    //

--- src/assignment/A1PB.java
+++ src/assignment/A1PB.java
@@ -14,9 +14,7 @@ public class A1PB {
 		System.out.println("Subtotal: "+ subTotal + "$");
 		System.out.println("Sales Tax (15%): " + salesTax);
 		System.out.println("Total: " + Math.ceil(Total));
-
 		//methods like Math.ceil or Math.pow etc. with capital letters in the beginning are methods already installed and done by Java
 		//so you don't have to write explanations for the methods below
 	}
-		
 }
