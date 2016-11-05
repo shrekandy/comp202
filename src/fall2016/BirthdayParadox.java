03cdc07a924a7b71a54e928d8c77fe0c80045a3c
Yan Ren <ryan1219@live.com>
2016-11-04 22:58:09 -0400

    Q1.4.5.6 finished

--- src/fall2016/BirthdayParadox.java
+++ src/fall2016/BirthdayParadox.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+package fall2016;
+
 import java.util.Random;
 
 public class BirthdayParadox {
@@ -12,12 +13,58 @@ public class BirthdayParadox {
 		// System.out.println(countElement(table, 19));
 
 	}
-
-	public static int countElement(int[][] dataTalbe, int element) {
+	
+	public static double runExperiment(int size) throws IllegalArgumentException{
+		int iterations = 200;
+		int range = 365;
+		int duplicatedArrays = 0;
+		
+		if(size == 1){
+			throw new IllegalArgumentException();
+		}
+		int[][] dates = generateAllData(iterations, size, range);
+		for(int i =0; i < dates.length; i++){
+			if(hasDuplicates(dates[i])){
+				duplicatedArrays++;
+			}
+		}
+		
+		return (double)duplicatedArrays / (double) iterations;
+		
+	}
+	
+	public static int maxDay(int[][] dataTable){
+		int frequecny = 0;
+		int mode  = 0;
+		
+		for (int i = 0; i < dataTable.length; i++) {
+			for (int j = 0; j < dataTable[i].length; j++) {
+				if(countElement(dataTable, dataTable[i][j]) > frequecny){
+					mode = dataTable[i][j];
+				}
+			}
+		}
+		return mode;
+	}
+	
+	public static boolean hasDuplicates(int[] input){
+		int[][] dataTable = new int[2][];
+		dataTable[0] = input.clone();
+		dataTable[1] = input.clone();
+		
+		for(int i = 0; i<dataTable[0].length; i++){
+			if(countElement(dataTable, dataTable[0][i]) != 2){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static int countElement(int[][] dataTable, int element) {
 		int count = 0;
-		for (int i = 0; i < dataTalbe.length; i++) {
-			for (int j = 0; j < dataTalbe[i].length; j++) {
-				if (dataTalbe[i][j] == element) {
+		for (int i = 0; i < dataTable.length; i++) {
+			for (int j = 0; j < dataTable[i].length; j++) {
+				if (dataTable[i][j] == element) {
 					count++;
 				}
 			}
