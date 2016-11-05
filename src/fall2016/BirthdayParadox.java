bc163d7734cbe0c69b80999d4522b88e8f0c3fc9
Yan Ren <ryan1219@live.com>
2016-11-05 18:13:17 -0400

    Q1.1-5 finished

--- src/fall2016/BirthdayParadox.java
+++ src/fall2016/BirthdayParadox.java
@@ -1,75 +1,111 @@
 package fall2016;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class BirthdayParadox {
 
 	public static void main(String[] args) {
-		// for(int i = 0; i < 10; i++){
-		// System.out.println(Arrays.toString(generateArray(20+i, 20)));
-		// }
-		// int[][] table = generateAllData(3, 6, 20);
-		// System.out.println(Arrays.deepToString(table));
-		// System.out.println(countElement(table, 19));
 
+		// for (int i = 0; i < 10; i++) {
+		// System.out.println(Arrays.toString(generateArray(20 + i, 20)));
+		// }
+
+		int[][] table = generateAllData(3, 5, 20);
+		System.out.println(Arrays.deepToString(table));
+
+		//System.out.println(countElement(table, 19));
+		
+		System.out.println(maxDay(table));
 	}
-	
-	public static double runExperiment(int size) throws IllegalArgumentException{
+
+	public static double runExperiment(int size) throws IllegalArgumentException {
 		int iterations = 200;
 		int range = 365;
 		int duplicatedArrays = 0;
-		
-		if(size == 1){
+
+		if (size == 1) {
 			throw new IllegalArgumentException();
 		}
 		int[][] dates = generateAllData(iterations, size, range);
-		for(int i =0; i < dates.length; i++){
-			if(hasDuplicates(dates[i])){
+		for (int i = 0; i < dates.length; i++) {
+			if (hasDuplicates(dates[i])) {
 				duplicatedArrays++;
 			}
 		}
-		
-		return (double)duplicatedArrays / (double) iterations;
-		
+
+		return (double) duplicatedArrays / (double) iterations;
+
 	}
-	
-	public static int maxDay(int[][] dataTable){
-		int frequecny = 0;
-		int mode  = 0;
-		
+
+	// try to use q3 method
+	public static int maxDay(int[][] dataTable) {
+		int mode = 0;
+		int frequencyOfMode = 0;
+
 		for (int i = 0; i < dataTable.length; i++) {
 			for (int j = 0; j < dataTable[i].length; j++) {
-				if(countElement(dataTable, dataTable[i][j]) > frequecny){
+				if (countElement(dataTable, dataTable[i][j]) > frequencyOfMode) {
 					mode = dataTable[i][j];
+					frequencyOfMode = countElement(dataTable, dataTable[i][j]);
 				}
 			}
 		}
 		return mode;
 	}
-	
-	public static boolean hasDuplicates(int[] input){
+
+	public static boolean hasDuplicates(int[] input) {
 		int[][] dataTable = new int[2][];
 		dataTable[0] = input.clone();
 		dataTable[1] = input.clone();
 		
-		for(int i = 0; i<dataTable[0].length; i++){
-			if(countElement(dataTable, dataTable[0][i]) != 2){
-				return true;
-			}
+		int mode = maxDay(dataTable);
+		if(countElement(dataTable, mode) > 2){
+			return true;
+		}else{
+			return false;
 		}
-		return false;
+		
 	}
-	
+
+	// not using q3 method
+	// public static int maxDay(int[][] dataTable) {
+	// int frequecny = 0;
+	// int mode = 0;
+	//
+	// for (int i = 0; i < dataTable.length; i++) {
+	// for (int j = 0; j < dataTable[i].length; j++) {
+	// if (countElement(dataTable, dataTable[i][j]) > frequecny) {
+	// mode = dataTable[i][j];
+	// }
+	// }
+	// }
+	// return mode;
+	// }
+
+//	public static boolean hasDuplicates(int[] input) {
+//		int[][] dataTable = new int[2][];
+//		dataTable[0] = input.clone();
+//		dataTable[1] = input.clone();
+//
+//		for (int i = 0; i < dataTable[0].length; i++) {
+//			if (countElement(dataTable, dataTable[0][i]) != 2) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+
 	public static int countElement(int[][] dataTable, int element) {
-		int count = 0;
+		int counter = 0;
 		for (int i = 0; i < dataTable.length; i++) {
 			for (int j = 0; j < dataTable[i].length; j++) {
 				if (dataTable[i][j] == element) {
-					count++;
+					counter++;
 				}
 			}
 		}
-		return count;
+		return counter;
 	}
 
 	public static int[][] generateAllData(int iterations, int size, int range) {
