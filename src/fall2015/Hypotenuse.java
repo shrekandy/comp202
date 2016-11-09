57a00a8a4b3a75f246d362b4da1a2fe00975e2ab
Ren, Yan <yan.ren@kronos.com>
2016-11-09 10:32:11 -0500

    fall2016 midterm long answer solution

---
+++ src/fall2015/Hypotenuse.java
@@ -0,0 +1,49 @@
+package fall2015;
+
+import java.util.Scanner;
+
+/*
+ * COMP-202A - Foundations of Computing (Fall 2015) - All Sections	Midterm Examination
+ * Long Answer Questions unofficial solution
+ */
+public class Hypotenuse {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double base, height;
+		System.out.println("Input the base:");
+		base = sc.nextDouble();
+		System.out.println("Input the height:");
+		height = sc.nextDouble();
+		System.out.println("The hypotenuse is " + squareRoot((power(base, 2) + power(height, 2))));
+		sc.close();
+	}
+
+	public static double absVal(double number) {
+		if (number < 0) {
+			number = number * -1;
+		}
+		return number;
+	}
+
+	public static double power(double x, int y) {
+		if (y == 0) {
+			return 1;
+		}
+		// using recursion, brute force method
+		// such as looping through and accumulate answer is also good
+		return (x * power(x, y - 1));
+	}
+
+	public static double squareRoot(double y) {
+		double error = 1e-1;
+		double s0 = y / 2;
+		double s1 = (s0 + y / s0) / 2;
+
+		while (absVal(s1 - s0) > error) {
+			s0 = s1;
+			s1 = (s1 + y / s1) / 2;
+		}
+
+		return s1;
+	}
+}
--- src/fall2016/AlphabetSoup.java
+++ src/fall2016/AlphabetSoup.java
@@ -1,8 +1,21 @@
 package fall2016;
+
 public class AlphabetSoup {
 
 	public static void main(String[] args) {
-
+		if(args.length < 2){
+			throw new IllegalArgumentException("args is less than two");
+		}else if(args[0].length() != 1){
+			throw new IllegalArgumentException("First String in args is not a single character");
+		}
+		
+		char key = args[0].charAt(0);
+		String[] stringList = copySubArray(args, 1, args.length);
+		if(haveChar(stringList, key)){
+			System.out.println("The letter "+ key+ " is present in all of the Strings");
+		}else{
+			System.out.println("Then letter " +  key + " is not present in all of the Strings");
+		}
 	}
 
 	public static int countChar(String str, char key) {
@@ -27,7 +40,7 @@ public class AlphabetSoup {
 	public static String[] copySubArray(String[] original, int startIndex, int endIndex) {
 		String[] subArray = new String[endIndex - startIndex + 1];
 		int indexOfSubArray = 0;
-		for(int i = startIndex; i < endIndex; i++){
+		for (int i = startIndex; i < endIndex; i++) {
 			subArray[indexOfSubArray] = original[i];
 			indexOfSubArray++;
 		}
